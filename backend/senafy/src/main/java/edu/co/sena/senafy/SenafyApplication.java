389f72d87f025a189c1a9867b529bdb1ff0687c8
Gustavo Rodriguez <txvoz@192.168.1.14>
2026-03-26 19:54:37 -0500

    add backend project

---
+++ backend/senafy/src/main/java/edu/co/sena/senafy/SenafyApplication.java
@@ -0,0 +1,13 @@
+package edu.co.sena.senafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenafyApplication.class, args);
+	}
+
+}
---
+++ backend/senafy/src/test/java/edu/co/sena/senafy/SenafyApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.co.sena.senafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenafyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
